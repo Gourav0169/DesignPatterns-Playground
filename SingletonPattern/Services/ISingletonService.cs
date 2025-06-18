742e6eddd3d0433b093f0f3fdf89924ee6985cb6
Gourav Dash <gourav.dash@spurtreetech.com>
2025-06-18 13:01:28 +0530

    Add project files.

---
+++ SingletonPattern/Services/ISingletonService.cs
@@ -0,0 +1,7 @@
+﻿namespace SingletonPattern.Services
+{
+	public interface ISingletonService
+	{
+		T SingletonInstance<T>(T t) where T : class;
+	}
+}
---
+++ SingletonPattern/Services/SingletonService.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Concurrent;
+
+namespace SingletonPattern.Services
+{
+	public class SingletonService : ISingletonService
+	{
+		private static ConcurrentDictionary<Type, Lazy<object>> _instance = new();
+
+		public T SingletonInstance<T>(T t) where T : class
+		{
+			var type =  typeof(T);
+			var lazyInstance = _instance.GetOrAdd(type, _ => new Lazy<object>(() => t, LazyThreadSafetyMode.ExecutionAndPublication));
+
+			return (T)lazyInstance.Value;
+		}
+	}
+}
